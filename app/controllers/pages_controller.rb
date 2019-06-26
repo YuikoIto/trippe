ae7682366eb582fa01dd743900c22af078f304d8
YuikoIto <toffywing@gmail.com>
2019-06-27 01:53:46 +0900

    add_style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def top
   end
-
   def start
+  end
+  def price
+  end
+  def place
+  end
+  def theme
+  end
+  def time
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#top'
   get '/start' => 'pages#start'
+  get '/price' => 'pages#price'
+  get '/place' => 'pages#place'
+  get '/theme' => 'pages#theme'
+  get '/time' => 'pages#time'
 end
